7a75d6d93d9d2d64b1d123ab0f585d4fb4874d4b
Louis Acresti <louis.acresti@gmail.com>
2011-10-28 13:54:23 -0400

    updated deploy script some more

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git checkout gh-pages
 rm -rf docs lib index.html
 mv tmp/* .
 rm -rf tmp
+mv live_test.html index.html
 git commit -am "auto-deployed to GitHub"
 git push origin gh-pages
 git checkout master
